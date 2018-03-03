ac03264f6a0af55ef83fcd0904666ff4bd1f6046
Juan Adarme <sebasa_t@hotmail.com>
2018-03-03 09:43:38 -0500

    Sistema solar - Sebastian Adarme

---
+++ SebastianAdarme-SolarSystem/Assets/SCRIPTS/Around.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Around : MonoBehaviour {
+	public Transform SUN;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (SUN.position, Vector3.up, speed);
+	}
+}
---
+++ SebastianAdarme-SolarSystem/Assets/SCRIPTS/ROTATION.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ROTATION : MonoBehaviour {
+
+	public float seed;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (0, 0.25f, 0);
+	}
+}
